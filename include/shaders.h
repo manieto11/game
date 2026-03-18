f36d4d688936d5379b429b636122a3a2080513be
manieto11 <nm17gb@gmail.com>
2026-03-18 08:16:48 +0100

    Add shaders

---
+++ include/shaders.h
@@ -0,0 +1,6 @@
+#ifndef SHADERS_H
+#define SHADERS_H
+
+extern const char *pixelizerFragmentShader;
+
+#endif
---
+++ src/shaders.cpp
@@ -0,0 +1,5 @@
+#include "shaders.h"
+
+const char *pixelizerFragmentShader = 
+    "#version 330\n"
+    "in vec2 fragTexCoord;\n"; // Stopped to make shader exporter (as side quest)
